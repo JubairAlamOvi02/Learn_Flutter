a274b9593745e924f47f0b7c5338186417688628
Jubair Alam Ovi <61586952+JubairAlamOvi02@users.noreply.github.com>
2021-10-12 20:54:42 +0600

    jokes

--- learn_flutter/lib/main.dart
+++ learn_flutter/lib/main.dart
@@ -1,33 +1,55 @@
 import 'package:flutter/material.dart';
 
-void main(List<String> args) {
-  runApp(
-    MyApp(),
-  );
+void main() {
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends StatefulWidget {
+  //const ({ Key? key }) : super(key: key);
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var count = 0;
+  var jokes = [
+    'Learn programming to understand programming jokes.',
+    'Code never lies,comments sometimes do.',
+    'You are semicolons to my Statements',
+    'Programming is 1% writing code and 99% understanding why its not working',
+    'I told him I cound not open jar. He told me to download java',
+    'Comparing java and javascript is same as comparing car and carpet',
+    'Golden rule of programming - If it works dont touch it.'
+  ];
+  void incrementCount() {
+    setState(() {
+      if (count == jokes.length - 1) {
+        count = 0;
+      } else {
+        count = count + 1;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Center(child: Text('ALHUMDULLHIA')),
-          backgroundColor: Colors.redAccent,
+          title: Text('BISMILLAHA'),
+          centerTitle: true,
+          backgroundColor: Colors.green[400],
         ),
         body: Center(
-            child: Text(
-          'ALLAH knows everyting',
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-        )),
+          child: Text(
+            '${jokes[count]}',
+            style: TextStyle(fontSize: 40),
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            print('clicked');
-          },
-          backgroundColor: Colors.redAccent,
+          onPressed: incrementCount,
           child: Icon(Icons.add),
+          backgroundColor: Colors.green[400],
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       ),
